713edf0bfe3add596284d6ce42a706d7c75175ba
Owen Cai <owenbcai@gmail.com>
2026-04-05 02:34:37 -0500

    under construction page

--- app/other-page/page.tsx
+++ app/other-page/page.tsx
@@ -3,9 +3,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
         <div className="pl-1 pr-1 flex flex-row items-center justify-between align-middle h-24 bg-[repeating-linear-gradient(45deg,#efefef_0_20px,#f5f5f5_20px_40px)] dark:bg-[repeating-linear-gradient(45deg,#090909_0_20px,#161616_20px_40px)]">
-            <div className="h-32 w-48 ">
-                test 2
-            </div>
+
         </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
-        <div className="pl-1 pr-1 flex flex-row items-center justify-between align-middle h-24 bg-[repeating-linear-gradient(45deg,#efefef_0_20px,#f5f5f5_20px_40px)] dark:bg-[repeating-linear-gradient(45deg,#090909_0_20px,#161616_20px_40px)]">
-            <div className="h-32 w-48 ">
-                test
+        <div className="rounded-xl flex flex-col justify-start border-2 overflow-hidden">
+            <div className="pl-2 pr-2 flex flex-row items-center justify-between align-middle h-12 bg-[repeating-linear-gradient(45deg,#dfdfdf_0_20px,#f5f5f5_20px_40px)] dark:bg-[repeating-linear-gradient(45deg,#090909_0_20px,#262626_20px_40px)]">
+                <p className="font-semibold text-2xl">Under Construction</p>
+            </div>
+            <div className="p-2">
+                <p className="w-0 min-w-full leading-snug">The ACPC Spring 2026 contest page is still under development.</p>
+                <p>Countdown here</p>
             </div>
         </div>
+
     </div>
   );
 }
